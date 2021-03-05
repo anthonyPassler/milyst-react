46938a15809abd1fa2d813c5cb37a3b66467bb02
anthonyPassler <passleranthony@yahoo.com>
2021-03-05 15:39:56 +0100

    Add react-router-dom and Lists component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './styles/App.scss';
 import LandingPage from "./components/LandingPage/LandingPage.js";
-
+import Lists from "./components/Lists/Lists.js";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Switch>
+        <Route path="/lists">
+          <Lists />
+        </Route>
+        <Route path="/">
+          <LandingPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
